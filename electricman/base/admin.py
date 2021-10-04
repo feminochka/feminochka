589adde534c2d1eb28ea5a4567eb82c8fa7877af
feminochka <masha1901@mail.ru>
2021-10-04 18:53:37 +0200

    Revert "откат"
    This reverts commit 7d9fb526

--- electricman/base/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from base.models import *
-
-# Register your models here.
-admin.site.register(AboutCategory)
-admin.site.register(Description)
--- electricman/main/views.py
+++ electricman/main/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from .models import *
+#from menu.models import *
 
 # Create your views here.
 
 
 def index(request):
-    about = AboutCategory.objects.all()
-    description = Description.objects.all()
-
-    data = {
-        'about': about,
-        'description': description,
-    }
-    return render(request, 'main/index.html', data)
+    # about = AboutCategory.objects.all()
+    # description = Description.objects.all()
+    #
+    # data = {
+    #     'about': about,
+    #     'description': description,
+    # }
+    return render(request, 'main/index.html',) #data)
